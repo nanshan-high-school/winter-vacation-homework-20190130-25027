083650eb20a74ff46e3fadd35a240a431dbfa3fd
25027劉修瑞 <44057587+25027@users.noreply.github.com>
2019-02-10 20:37:34 +0800

    Create a065.cpp

---
+++ a065.cpp
@@ -0,0 +1,22 @@
+# winter-vacation-homework-20190130-25027
+winter-vacation-homework-20190130-25027 created by GitHub Classroom
+
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    int new_code = 0;
+    string code;
+    cout << "請輸入7個大寫字母:";
+    cin >> code;
+    for (int i = 0; i < 6; i++) {
+        if (code[i] > code[i + 1]) {
+           number = code[i] - code[i + 1];
+        }
+        else {
+             number = code[i + 1] - code[i];
+        }
+        new_code = new_code * 10 + number;
+    }
+    cout << new_code << "\n";
+}
